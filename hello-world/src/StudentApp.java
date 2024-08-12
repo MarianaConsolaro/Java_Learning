13459aa8e67f5b3bd252c743d7ea57ab5ec95256
Mariana <mariana.perez.consolaro@gmail.com>
2024-08-12 10:44:30 -0300

    Concatenating Strings

--- hello-world/src/StudentApp.java
+++ hello-world/src/StudentApp.java
@@ -5,15 +5,26 @@ public class StudentApp {
         String initialLastName = "C";
         double media = 10.0;
         boolean approved = true;
-        String name = "Mariana Consolaro";
+        String name = "Mariana";
+        String lastName = "Consolaro";
 
         System.out.println(age);
         System.out.println(initialName);
         System.out.println(initialLastName);
         System.out.println(media);
         System.out.println(approved);
-        System.out.println(name);
+        System.out.println(name + " " + lastName);
+        System.out.println(initialName + " " + initialLastName);
         //printing a text and not a variable
         System.out.println("Mariana");
+
+        System.out.println(
+            age + ", " 
+            + initialName + ", " 
+            + initialLastName + ", " 
+            + media + ", " 
+            + approved + ", " 
+            + name + " " + lastName
+        );
     }
 }
