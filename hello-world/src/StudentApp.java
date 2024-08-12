2246fbe61c2adfb919cd8c76e196533b53dc5419
Mariana <mariana.perez.consolaro@gmail.com>
2024-08-12 10:32:30 -0300

    Student

---
+++ hello-world/src/StudentApp.java
@@ -0,0 +1,15 @@
+public class StudentApp {
+    public static void main(String[] args) throws Exception {
+        int age = 18;
+        String initialName = "M";
+        String initialLastName = "C";
+        double media = 10.0;
+        boolean approved = true;
+
+        System.out.println(age);
+        System.out.println(initialName);
+        System.out.println(initialLastName);
+        System.out.println(media);
+        System.out.println(approved);
+    }
+}
