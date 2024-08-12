f1741e13e57d31979d088befdc247aa05d2f92d2
Mariana <mariana.perez.consolaro@gmail.com>
2024-08-12 10:36:14 -0300

    Lesson 1 - String

--- hello-world/src/StudentApp.java
+++ hello-world/src/StudentApp.java
@@ -5,11 +5,15 @@ public class StudentApp {
         String initialLastName = "C";
         double media = 10.0;
         boolean approved = true;
+        String name = "Mariana Consolaro";
 
         System.out.println(age);
         System.out.println(initialName);
         System.out.println(initialLastName);
         System.out.println(media);
         System.out.println(approved);
+        System.out.println(name);
+        //printing a text and not a variable
+        System.out.println("Mariana");
     }
 }
