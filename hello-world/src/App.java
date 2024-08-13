a7cdc67aa308b15232773dd1bc75f854ed11a45a
Mariana <mariana.perez.consolaro@gmail.com>
2024-08-13 16:26:06 -0300

    Studemt

--- hello-world/src/App.java
+++ hello-world/src/App.java
@@ -1,25 +1,43 @@
 public class App {
 
     public static void main(String[] args) {
-        User user1 = new User(
-            "mconsolaro",
-            "mari.consolaro",
-            "mariana@gmail.com.br"            
-        );
+        Student student1 = new Student (
+            "Estudante 1", 2025, 10.0);
+            Student student2 = new Student (
+            "Estudante 2", 2024, 8.0);
 
-        User user2 = new User(
-            "user2",
-            "user2",
-            "user2@gmail.com.br"
-        );
-
-        user1.active = true;
-        //System.out.println(user1.isActive());
-        //System.out.println(user2.isActive());
-        //System.out.println(User.getCharacterLimit());
-  
-        System.out.println(user1.username);
-        System.out.println(user2.username);
-        System.out.println(User.NUMBER_OF_CHARACTERS_USER);
+            student1.setYearGraduation(2026);
+            System.out.println(student1.yearGraduation);
+            System.out.println(student2.yearGraduation);
     }
+
 }
+
+
+
+
+
+
+    // public static void main(String[] args) {
+    //     User user1 = new User(
+    //         "mconsolaro",
+    //         "mari.consolaro",
+    //         "mariana@gmail.com.br"            
+    //     );
+
+    //     User user2 = new User(
+    //         "user2",
+    //         "user2",
+    //         "user2@gmail.com.br"
+    //     );
+
+    //     user1.active = true;
+    //     //System.out.println(user1.isActive());
+    //     //System.out.println(user2.isActive());
+    //     //System.out.println(User.getCharacterLimit());
+  
+    //     System.out.println(user1.username);
+    //     System.out.println(user2.username);
+    //     System.out.println(User.NUMBER_OF_CHARACTERS_USER);
+    // }
+
---
+++ hello-world/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+ public String name;
+ public int yearGraduation;
+ public double media;
+
+ public Student(String name, int yearGraduation, double media) {
+    this.name = name;
+    this.yearGraduation = yearGraduation;
+    this.media = media;
+ }
+
+ public void setYearGraduation(int yearGraduation) {
+    this.yearGraduation = yearGraduation;
+ }
+
+}
